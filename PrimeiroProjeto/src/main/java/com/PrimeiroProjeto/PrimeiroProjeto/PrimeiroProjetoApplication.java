b34234d1a8137071b8a0e80030981adfe6078a61
Lucas Mateus <lukinhasm07@outlook.com>
2018-12-16 22:11:26 -0300

    Projeto inicial

--- PrimeiroProjeto/src/main/java/com/PrimeiroProjeto/PrimeiroProjeto/PrimeiroProjetoApplication.java
+++ PrimeiroProjeto/src/main/java/com/PrimeiroProjeto/PrimeiroProjeto/PrimeiroProjetoApplication.java
@@ -1,13 +1,13 @@
-package com.ProjetoInicial.ProjetoInicial;
+package com.PrimeiroProjeto.PrimeiroProjeto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjetoInicialApplication {
+public class PrimeiroProjetoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProjetoInicialApplication.class, args);
+		SpringApplication.run(PrimeiroProjetoApplication.class, args);
 	}
 
 }
--- PrimeiroProjeto/src/test/java/com/PrimeiroProjeto/PrimeiroProjeto/PrimeiroProjetoApplicationTests.java
+++ PrimeiroProjeto/src/test/java/com/PrimeiroProjeto/PrimeiroProjeto/PrimeiroProjetoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ProjetoInicial.ProjetoInicial;
+package com.PrimeiroProjeto.PrimeiroProjeto;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ProjetoInicialApplicationTests {
+public class PrimeiroProjetoApplicationTests {
 
 	@Test
 	public void contextLoads() {
