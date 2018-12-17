49e9361778df1dc2611c1b5f77dbee0631a802f2
Lucas Mateus <lukinhasm07@outlook.com>
2018-12-16 21:47:20 -0300

    commite

---
+++ ProjetoInicial/src/main/java/com/ProjetoInicial/ProjetoInicial/ProjetoInicialApplication.java
@@ -0,0 +1,14 @@
+package com.ProjetoInicial.ProjetoInicial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoInicialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoInicialApplication.class, args);
+	}
+
+}
+
